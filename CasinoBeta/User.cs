5c1dc8a24997000fd2a09fa788c7be001a8eb980
Dániel Palman <palman.daniel47@gmail.com>
2021-04-06 13:40:44 +0200

    User osztály elkészítése

--- CasinoBeta/User.cs
+++ CasinoBeta/User.cs
@@ -8,5 +8,46 @@ namespace CasinoBeta
 {
     class User
     {
+        private string nev;
+        public string Nev
+        {
+            get { return nev; }
+            set { nev = value; }
+        }
+
+
+
+        private string jelszo;
+        public string Jelszo
+        {
+            get { return jelszo; }
+            set { jelszo = value; }
+        }
+
+
+
+        private string jogkor;
+        public string Jogkor
+        {
+            get { return jogkor; }
+            set { jogkor = value; }
+        }
+
+
+
+        private string teljesNev;
+        public string TeljesNev
+        {
+            get { return teljesNev; }
+            set { teljesNev = value; }
+        }
+
+        public User(string nev, string jelszo, string jogkor, string teljesNev)
+        {
+            this.nev = nev;
+            this.jelszo = jelszo;
+            this.jogkor = jogkor;
+            this.teljesNev = teljesNev;
+        }
     }
 }
